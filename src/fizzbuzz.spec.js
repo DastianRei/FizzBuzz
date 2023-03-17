a6d63f14d5461a919261a474302df5c39fef135a
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-17 10:05:05 -0400

    escribimos el test de 3 tal que nos tiene que devolver Fizz por que es multiplo de 3

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -2,10 +2,13 @@ import fizzbuzz from "./fizzbuzz";
 
 
 describe("Fizz Buzz", () => {
-  it("Nada", () => {
+  it("genera 1 en cadena", () => {
     expect(fizzbuzz(1)).toEqual("1");
   });
-  it("Nada", () => {
+  it("genera 2 en cadena", () => {
     expect(fizzbuzz(2)).toEqual("2");
+  });
+  it("genera 2 en cadena", () => {
+    expect(fizzbuzz(3)).toEqual("Fizz");
   });
 });
