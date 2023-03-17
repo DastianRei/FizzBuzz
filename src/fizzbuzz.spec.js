d4a479e68faa9f62fc30f39a784d3cb473900d86
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-17 10:11:09 -0400

    escribimos el test de 5 tal que nos tiene que devolver Buzz por que es multiplo de 5

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -8,7 +8,13 @@ describe("Fizz Buzz", () => {
   it("genera 2 en cadena", () => {
     expect(fizzbuzz(2)).toEqual("2");
   });
-  it("genera 2 en cadena", () => {
+  it("genera 3 en cadena", () => {
     expect(fizzbuzz(3)).toEqual("Fizz");
+  });
+  it("genera 4 en cadena", () => {
+    expect(fizzbuzz(4)).toEqual("4");
+  });
+  it("genera 5 en cadena", () => {
+    expect(fizzbuzz(5)).toEqual("Buzz");
   });
 });
