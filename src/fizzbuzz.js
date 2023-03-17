f585cf64edb4862adf18f686e05086ea11d067d5
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-17 10:22:52 -0400

    comprobamos con multiplos de 3 y 5 que funcione de manera correcta

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -2,10 +2,10 @@ function fizzbuzz(num) {
   if (num % 3 == 0 && num % 5 == 0) {
     return "FizzBuzz";
   }
-  if (num == 5) {
+  if (num % 5 == 0) {
     return "Buzz";
   }
-  if (num == 3) {
+  if (num % 3 == 0) {
     return "Fizz";
   }
   return num.toString();
