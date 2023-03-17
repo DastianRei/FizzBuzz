010408b3da9571778ae9aced1927e6ad31da560b
Dayan <dayan.aguilar@ucb.edu.bo>
2023-03-17 10:11:45 -0400

    corregimos la funcion fizzbuzz en el archivo fizzbuzz.js de tal manera que pase la prueba del 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,8 +1,11 @@
-function fizzbuzz(num){
-    if(num == 3){
-        return "Fizz";
-    }
-    return num.toString();
+function fizzbuzz(num) {
+  if (num == 5) {
+    return "Buzz";
+  }
+  if (num == 3) {
+    return "Fizz";
+  }
+  return num.toString();
 }
 
 export default fizzbuzz;
